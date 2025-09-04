ea5e7c44a81db48eeb6e315c1a16ca8142616ccf
Bruno Pressi <brunopressi2012@gmail.com>
2025-09-04 16:01:50 -0300

    feat: add Cliente and Endereco entities

---
+++ src/main/java/com/bruno/pressi/aluguelveiculos/entities/Cliente.java
@@ -0,0 +1,36 @@
+package com.bruno.pressi.aluguelveiculos.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "clientes")
+public class Cliente {
+
+    @Id
+    private Long id;
+
+    @Field("nome_completo")
+    private String nomeCompleto;
+
+    @Indexed(unique = true)
+    private String email;
+
+    @Indexed(unique = true)
+    private String cpf;
+
+    @Field("numero_cnh")
+    @Indexed(unique = true)
+    private int numeroCNH;
+    private String telefone;
+
+    private Endereco endereco;
+
+}
---
+++ src/main/java/com/bruno/pressi/aluguelveiculos/entities/Endereco.java
@@ -0,0 +1,19 @@
+package com.bruno.pressi.aluguelveiculos.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Endereco {
+
+    private String cep;
+    private String cidade;
+    private String estado;
+    private String rua;
+    private String bairro;
+    private String complemento;
+
+}
