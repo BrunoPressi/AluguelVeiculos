00bbb37327714cff0cc9fbabc098abee18d33e89
Bruno Pressi <brunopressi2012@gmail.com>
2025-09-04 15:16:08 -0300

    add starter project

---
+++ src/main/java/com/bruno/pressi/aluguelveiculos/AluguelVeiculosApplication.java
@@ -0,0 +1,13 @@
+package com.bruno.pressi.aluguelveiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluguelVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluguelVeiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bruno/pressi/aluguelveiculos/AluguelVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.pressi.aluguelveiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluguelVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
