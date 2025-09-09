2eb3733590ddac5b66a7c75520914fd428637bab
Bruno Pressi <brunopressi2012@gmail.com>
2025-09-09 14:39:11 -0300

    feat: add createCliente endpoint

--- src/main/java/com/bruno/pressi/aluguelveiculos/services/ClienteService.java
+++ src/main/java/com/bruno/pressi/aluguelveiculos/services/ClienteService.java
@@ -1,13 +1,37 @@
 package com.bruno.pressi.aluguelveiculos.services;
 
+import com.bruno.pressi.aluguelveiculos.entities.Cliente;
+import com.bruno.pressi.aluguelveiculos.exceptions.DuplicateClienteException;
 import com.bruno.pressi.aluguelveiculos.repositories.ClienteRepository;
+import com.bruno.pressi.aluguelveiculos.web.dto.ClienteDTO.ClienteCreateDto;
+import com.bruno.pressi.aluguelveiculos.web.dto.ClienteDTO.ClienteResponseDto;
+import com.bruno.pressi.aluguelveiculos.web.dto.mapper.ObjectMapper;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ClienteService {
 
     private final ClienteRepository clienteRepository;
 
+    @Transactional(readOnly = false)
+    public ClienteResponseDto saveCliente(ClienteCreateDto clienteCreateDto) {
+        Cliente cliente = ObjectMapper.parseObject(clienteCreateDto, Cliente.class);
+
+        try {
+            clienteRepository.insert(cliente);
+        }
+        catch (org.springframework.dao.DuplicateKeyException e) {
+            String msg = e.getMessage();
+            String details = msg.substring(msg.indexOf("dup key: ") + 8, msg.indexOf("details") - 3).trim();
+            throw new DuplicateClienteException("Cliente já registrado: " + details);
+        }
+
+        return ObjectMapper.parseObject(cliente, ClienteResponseDto.class);
+    }
 }
--- src/main/java/com/bruno/pressi/aluguelveiculos/web/controllers/ClienteController.java
+++ src/main/java/com/bruno/pressi/aluguelveiculos/web/controllers/ClienteController.java
@@ -1,13 +1,36 @@
 package com.bruno.pressi.aluguelveiculos.web.controllers;
 
 import com.bruno.pressi.aluguelveiculos.services.ClienteService;
+import com.bruno.pressi.aluguelveiculos.web.dto.ClienteDTO.ClienteCreateDto;
+import com.bruno.pressi.aluguelveiculos.web.dto.ClienteDTO.ClienteResponseDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/v1/cliente")
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+@Slf4j
+@RestController()
+@RequestMapping("/api/v1/cliente")
 @RequiredArgsConstructor
 public class ClienteController {
 
     private final ClienteService clienteService;
+
+    @PostMapping
+    public EntityModel<ClienteResponseDto> createCliente(@RequestBody @Valid ClienteCreateDto clienteCreateDto) {
+        ClienteResponseDto clienteResponseDto = clienteService.saveCliente(clienteCreateDto);
+        //Link selfLink = linkTo(methodOn(ClienteController.class).getUser(id)).withSelfRel();
+
+        return EntityModel.of(clienteResponseDto);
+    }
 
 }
