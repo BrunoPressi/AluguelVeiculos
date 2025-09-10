2628a65c68b238368a7361a106a6003448abda2e
Bruno Pressi <brunopressi2012@gmail.com>
2025-09-10 14:44:45 -0300

    feat: add VeiculoCreateDto and VeiculoResponseDto classes

--- src/main/java/com/bruno/pressi/aluguelveiculos/web/dto/VeiculoDTO/VeiculoResponseDto.java
+++ src/main/java/com/bruno/pressi/aluguelveiculos/web/dto/VeiculoDTO/VeiculoResponseDto.java
@@ -1,27 +1,21 @@
-package com.bruno.pressi.aluguelveiculos.entities;
+package com.bruno.pressi.aluguelveiculos.web.dto.VeiculoDTO;
 
 import com.bruno.pressi.aluguelveiculos.entities.enums.VeiculoStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Document(collection = "veiculos")
-public class Veiculo {
+public class VeiculoResponseDto {
 
-    @Id
     private String id;
     private String modelo;
     private String marca;
     private String ano;
     private String cor;
-
-    @Indexed(unique = true)
     private String placa;
     private VeiculoStatus status;
+
 }
