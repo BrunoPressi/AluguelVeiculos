2e7cb9775f775ae8a6ee72e447475e2b41b1cad9
Bruno Pressi <brunopressi2012@gmail.com>
2025-09-11 14:57:01 -0300

    fix: fix AluguelCreateDto

--- src/main/java/com/bruno/pressi/aluguelveiculos/web/dto/AluguelDTO/AluguelCreateDto.java
+++ src/main/java/com/bruno/pressi/aluguelveiculos/web/dto/AluguelDTO/AluguelCreateDto.java
@@ -1,9 +1,7 @@
 package com.bruno.pressi.aluguelveiculos.web.dto.AluguelDTO;
 
 import com.bruno.pressi.aluguelveiculos.entities.Veiculo;
-import jakarta.validation.constraints.Future;
-import jakarta.validation.constraints.FutureOrPresent;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,5 +21,14 @@ public class AluguelCreateDto {
 
     @Future(message = "A data de início deve ser no futuro.")
     private LocalDate dataFim;
+
+    @NotBlank(message = "Método de pagamento inválido, tente novamente.")
+    @Pattern(regexp = "^(PIX|BOLETO|CARTAO_CREDITO|CARTAO_DEBITO)$", message = "Método de pagamento deve ser PIX, BOLETO, CARTAO_CREDITO OU CARTAO_DEBITO.")
+    private String metodoPagamento;
+
+    @NotNull(message = "Número de parcelas inválido, tente novamente.")
+    @Min(value = 1, message = "O mínimo de parcelas é 1 (uma).")
+    @Max(value = 12, message = "O máximo de parcelas é 12 (doze).")
+    private int numeroParcelas;
 
 }
